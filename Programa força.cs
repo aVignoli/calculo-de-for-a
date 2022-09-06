d266569fa35594cc77e58e6d3e7362f306aa6bce
aVignoli <111579411+aVignoli@users.noreply.github.com>
2022-09-06 15:31:54 -0300

    Add files via upload
    programa em c# sobre calculo de força

---
+++ Programa força.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoIntegradorMatemática
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double força;
+            double massa;
+            double aceleracao;
+
+            Console.WriteLine("INSIRA A MASSA");
+            massa = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("INSIRA A ACELERACAO");
+            aceleracao = Convert.ToDouble(Console.ReadLine());
+
+
+            força = (massa * aceleracao);
+            Console.WriteLine(força);
+
+            Console.ReadKey();
+        }
+    }
+}
